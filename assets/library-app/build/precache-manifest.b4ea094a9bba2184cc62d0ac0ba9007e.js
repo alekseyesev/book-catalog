4bd9ab72fd61ab918254cb15234c9adcf8ab8d7a
alekseyesev <alekseyesev@live.ru>
2020-03-29 01:07:47 +0300

    Story 1: Add production version of React library

---
+++ assets/library-app/build/precache-manifest.b4ea094a9bba2184cc62d0ac0ba9007e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e125027514883ceb7c02dd1ec6616d8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "523be4333b9e46d4319e",
+    "url": "/static/css/main.d3035100.chunk.css"
+  },
+  {
+    "revision": "cb3057f83bde7d3b6639",
+    "url": "/static/js/2.26cece55.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.26cece55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "523be4333b9e46d4319e",
+    "url": "/static/js/main.6b583132.chunk.js"
+  },
+  {
+    "revision": "5df445b59343b86d1052",
+    "url": "/static/js/runtime-main.a7e0f762.js"
+  }
+]);
